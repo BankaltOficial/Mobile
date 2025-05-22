52f597ed076827f27ff5fedd6091e0d2f9981440
aluno <cl203230@g.unicamp.br>
2025-05-22 11:51:29 -0300

    arrumando icons

--- lib/InvestimentoScreen.dart
+++ lib/InvestimentoScreen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/WelcomeScreen.dart';
 import 'inicialScreen.dart';
+import 'InvestirScreen.dart';
 
 class InvestimentoScreen extends StatefulWidget {
   const InvestimentoScreen({super.key});
@@ -95,29 +97,104 @@ class _InvestimentoScreenState extends State<InvestimentoScreen> {
             ),
             SizedBox(height: 30),
             InkWell(
-              onTap: () {},
+              onTap: () {
+                Navigator.pushReplacement(
+                context, 
+                MaterialPageRoute(builder: (context) => const WelcomeScreen(),));
+              },
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                  CircleAvatar(
+                    backgroundColor: mainPurple,
+                    radius: 45,
+                    child: Icon(
+                      Icons.attach_money,
+                      color: mainWhite,
+                      size: 45,
+                      ),
+                  ),
+                  SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Text("Investir", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                    SizedBox(height: 10,),
+                    Text("Aplique seu dinheiro de forma segura e prática,\npara alcançar seus objetivos financeiros.", style: TextStyle(fontSize: 14),)
+                    ]
+                    ,)
+                ],),
+              ),
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                Navigator.pushReplacement(
+                context, 
+                MaterialPageRoute(builder: (context) => const WelcomeScreen(),));
+              },
+              child: Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                  CircleAvatar(
+                    backgroundColor: mainPurple,
+                    radius: 45,
+                    child: Icon(
+                      Icons.person,
+                      color: mainWhite,
+                      size: 50,
+                      ),
+                  ),
+                  SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Text("Perfil de investidor", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                    SizedBox(height: 10,),
+                    Text("Identifique seu perfil financeiro, recomendando \ninvestimentos alinhados ao seu nível de \nexperiência e tolerância ao risco.", style: TextStyle(fontSize: 14),)
+                    ]
+                    ,)
+                ],),
+              ),
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                Navigator.pushReplacement(
+                context, 
+                MaterialPageRoute(builder: (context) => const WelcomeScreen(),));
+              },
               child: Container(
                 padding: EdgeInsets.all(20),
                 child: Row(
                   children: [
                   CircleAvatar(
                     backgroundColor: mainPurple,
-                    radius: 40,
+                    radius: 45,
                     child: Icon(
                       Icons.hide_image,
                       color: mainWhite,
-                      size: 40,
+                      size: 45,
                       ),
                   ),
-                  Column(children: [
-                    Text("Investir", style: TextStyle(fontWeight: FontWeight.bold),),
+                  SizedBox(width: 20),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Text("Resgate", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
                     SizedBox(height: 10,),
-                    Text("")
+                    Text("Retirar o valor investido de forma rápida e \nfácil, diretamente para a sua conta.", style: TextStyle(fontSize: 14),)
                     ]
                     ,)
                 ],),
               ),
-            )
+            ),
         ],
       ),
       )

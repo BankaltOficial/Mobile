daba007c7b26163b4ac1124b27c74e8d5241c99e
Nucleo de Informatica <ni@cotil.unicamp.br>
2025-05-23 09:56:30 -0300

    consertando appbar

--- lib/InvestimentoScreen.dart
+++ lib/InvestimentoScreen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/PixScreen.dart';
 import 'package:flutter_application_1/WelcomeScreen.dart';
 import 'inicialScreen.dart';
 import 'InvestirScreen.dart';
@@ -13,53 +14,114 @@ class InvestimentoScreen extends StatefulWidget {
 }
 
 class _InvestimentoScreenState extends State<InvestimentoScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         toolbarHeight: 100,
         backgroundColor: mainPurple,
         elevation: 0,
-        title:Column(children: [ 
-          Row(  
+        automaticallyImplyLeading: false,
+        title: Column(
           children: [
-          IconButton(onPressed: () {
-            Navigator.pushReplacement(
-              context, 
-              MaterialPageRoute(builder: (context) => const InicialScreen(),));
-          }, icon: Icon(
-            Icons.menu,
-            color: mainWhite,
-            )
-          ),
-            Expanded(child: Center(child:
-            Text(
-            'bankalt',
-            style: TextStyle(
-            color: mainWhite,
-            fontWeight: FontWeight.bold
-          ),
-        ),),),
-        ],
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.menu, color: mainWhite),
+                  onPressed: () {
+                    _scaffoldKey.currentState?.openDrawer();
+                  },
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      'bankalt',
+                      style: TextStyle(
+                        color: mainWhite,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 48),
+              ],
+            ),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back, color: mainWhite),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const InicialScreen()),
+                    );
+                  },
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      'Investimentos',
+                      style: TextStyle(
+                        color: mainWhite,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 48),
+              ],
+            ),
+          ],
         ),
-        SizedBox(height:15),
-        Row(children: [ 
-          IconButton(onPressed: () {
-            Navigator.pushReplacement(
-              context, 
-              MaterialPageRoute(builder: (context) => const InicialScreen(),));
-          }, icon: Icon(
-            Icons.arrow_back,
-            color: mainWhite,
-            )
-          ),
-          Expanded(child: Center(child:
-          Text(
-          'Investimentos',
-          style: TextStyle(color: mainWhite),
-        ),),),
-         ],)
-        ]
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: mainPurple,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: mainWhite,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home_filled),
+              title: Text('Página inicial'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const InicialScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Image.asset("assets/icons/Pix.png", width: 20, height: 20),
+              title: Text('PIX'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const PixScreen()),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('Item 3'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
       body: SingleChildScrollView(
@@ -99,8 +161,9 @@ class _InvestimentoScreenState extends State<InvestimentoScreen> {
             InkWell(
               onTap: () {
                 Navigator.pushReplacement(
-                context, 
-                MaterialPageRoute(builder: (context) => const WelcomeScreen(),));
+                  context,
+                  MaterialPageRoute(builder: (context) => const InvestirScreen()),
+                );
               },
               child: Container(
                 width: double.infinity,
@@ -108,96 +171,124 @@ class _InvestimentoScreenState extends State<InvestimentoScreen> {
                 padding: EdgeInsets.all(20),
                 child: Row(
                   children: [
-                  CircleAvatar(
-                    backgroundColor: mainPurple,
-                    radius: 45,
-                    child: Icon(
-                      Icons.attach_money,
-                      color: mainWhite,
-                      size: 45,
+                    CircleAvatar(
+                      backgroundColor: mainPurple,
+                      radius: 45,
+                      child: Icon(
+                        Icons.attach_money,
+                        color: mainWhite,
+                        size: 45,
                       ),
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                    Text("Investir", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
-                    SizedBox(height: 10,),
-                    Text("Aplique seu dinheiro de forma segura e prática,\npara alcançar seus objetivos financeiros.", style: TextStyle(fontSize: 14),)
-                    ]
-                    ,)
-                ],),
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Investir",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Aplique seu dinheiro de forma segura e prática,\npara alcançar seus objetivos financeiros.",
+                          style: TextStyle(fontSize: 14),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
             SizedBox(height: 20),
             InkWell(
               onTap: () {
                 Navigator.pushReplacement(
-                context, 
-                MaterialPageRoute(builder: (context) => const WelcomeScreen(),));
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const WelcomeScreen()),
+                );
               },
               child: Container(
                 padding: EdgeInsets.all(20),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                  CircleAvatar(
-                    backgroundColor: mainPurple,
-                    radius: 45,
-                    child: Icon(
-                      Icons.person,
-                      color: mainWhite,
-                      size: 50,
+                    CircleAvatar(
+                      backgroundColor: mainPurple,
+                      radius: 45,
+                      child: Icon(
+                        Icons.person,
+                        color: mainWhite,
+                        size: 50,
                       ),
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                    Text("Perfil de investidor", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
-                    SizedBox(height: 10,),
-                    Text("Identifique seu perfil financeiro, recomendando \ninvestimentos alinhados ao seu nível de \nexperiência e tolerância ao risco.", style: TextStyle(fontSize: 14),)
-                    ]
-                    ,)
-                ],),
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Perfil de investidor",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Identifique seu perfil financeiro, recomendando \ninvestimentos alinhados ao seu nível de \nexperiência e tolerância ao risco.",
+                          style: TextStyle(fontSize: 14),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
             SizedBox(height: 20),
             InkWell(
               onTap: () {
                 Navigator.pushReplacement(
-                context, 
-                MaterialPageRoute(builder: (context) => const WelcomeScreen(),));
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const WelcomeScreen()),
+                );
               },
               child: Container(
                 padding: EdgeInsets.all(20),
                 child: Row(
                   children: [
-                  CircleAvatar(
-                    backgroundColor: mainPurple,
-                    radius: 45,
-                    child: Icon(
-                      Icons.hide_image,
-                      color: mainWhite,
-                      size: 45,
+                    CircleAvatar(
+                      backgroundColor: mainPurple,
+                      radius: 45,
+                      child: Icon(
+                        Icons.hide_image,
+                        color: mainWhite,
+                        size: 45,
                       ),
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                    Text("Resgate", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
-                    SizedBox(height: 10,),
-                    Text("Retirar o valor investido de forma rápida e \nfácil, diretamente para a sua conta.", style: TextStyle(fontSize: 14),)
-                    ]
-                    ,)
-                ],),
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Resgate",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Retirar o valor investido de forma rápida e \nfácil, diretamente para a sua conta.",
+                          style: TextStyle(fontSize: 14),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-        ],
+          ],
+        ),
       ),
-      )
     );
   }
 }
---
+++ lib/InvestirScreen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class InvestirScreen extends StatefulWidget {
+  const InvestirScreen({super.key});
+
+  @override
+  State<InvestirScreen> createState() => _InvestirScreenState();
+}
+
+class _InvestirScreenState extends State<InvestirScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
